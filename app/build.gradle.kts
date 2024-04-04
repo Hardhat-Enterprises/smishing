a25d1016270bee40fe4df89eee395bb4ddc043c1
Oliver <oliver.tait2222@gmail.com>
2024-04-04 15:45:59 +1100

    Addition of the AccountActivity/ Account settings page. Activity is blank needs to be completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.annotation)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
