7fe3e2a0dc7642cc1a28f69d08130e416ff1fbe3
gladizgregory <ggregory@deakin.edu.au>
2024-04-29 12:51:28 +1000

    MachineLearningDev

--- MachineLearningDev/TeaParty.py
+++ MachineLearningDev/TeaParty.py
@@ -31,7 +31,12 @@ all_predictions = []
 print("Let's vote: ")
 for name, model in models:
     pipeline = ModelPipeline(model)
-    pipeline.input_message = ['Please Stay At Home. To encourage the notion of staying at home. All tax-paying citizens are entitled to ï¿½305.96 or more emergency refund. smsg.io/fCVbD']
+    pipeline.input_message = ['Please Stay At Home. To encourage the notion of staying at home. All tax-paying citizens are entitled to ï¿½305.96 or more emergency refund. smsg.io/fCVbD'
+                              "Hello, you still have a fine that has not been paid. Please pay it in time, otherwise it will affect your travel. https://linkstps.xyz/au",
+                              "Eastlink: There is an outstanding debt on the toll invoice. Settlement should always be made before the maturity datae. https://tolls.eastlink.click/online",
+                              "We're unable to deliver your online package due to an address error. Please click promptly to update the address for re-delivery. https://ausorriso.xyz/i",
+                              "TODAY ONLY 40% off traditional and premium pizzas* ORDER NOW dominoes.au/7MKXrbmdRk T&Cs apply. To opt out send STOP to 0485865365",
+                              "Chemist Warehouse Fountain Gate sent you a Slyp receipt. View it here: https://reciepts.slyp.com.au/WRA-b94511379edf4de481438e-f4a6e952c0",]
     pipeline.run_model_pipeline(0.2, 100, 1)
     all_predictions.append((name, pipeline.prediction[0]))
 
