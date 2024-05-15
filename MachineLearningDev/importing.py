74b80b9f4289bc83fbe06a7bce257ae184ae3f84
Natasha <38268376+Natasji@users.noreply.github.com>
2024-05-15 18:18:31 +1000

    a pre trained model, but doesn't work on mine

--- MachineLearningDev/importing.py
+++ MachineLearningDev/importing.py
@@ -3,7 +3,8 @@ from list_of_functions import *
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-#import url model from transformers
+# Not working, something wrong with torch but don't know how to fix
+# Import url model from transformers
 
 tokenizer = AutoTokenizer.from_pretrained("kmack/malicious-url-detection")
 model = AutoModelForSequenceClassification.from_pretrained("kmack/malicious-url-detection")
