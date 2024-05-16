7bc3cc84fec2eb5ecceb034882f614c6d3fcddf4
ulindu7825 <ulinduperera@live.com>
2024-05-16 23:48:02 +1000

    Add SMS message detail Activity.

---
+++ app/src/main/java/com/example/smishingdetectionapp/SMSMessageDetailActivity.java
@@ -0,0 +1,42 @@
+package com.example.smishingdetectionapp;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.smishingdetectionapp.sms.model.SMSMessage;
+
+public class SMSMessageDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sms_message_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageButton back_btn = findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(v -> {
+            finish();
+        });
+
+        TextView text_sender = findViewById(R.id.text_sender);
+        TextView text_body = findViewById(R.id.text_body);
+
+        SMSMessage message = getIntent().getParcelableExtra("SMS_MESSAGE");
+        if (message != null) {
+            text_sender.setText(message.getSender());
+            text_body.setText(message.getBody());
+        }
+    }
+}
