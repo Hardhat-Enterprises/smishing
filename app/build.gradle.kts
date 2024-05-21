83b96c7adde532edab4f5667f2bc94d255706521
JohnJackson685 <jackstjepanovic@gmail.com>
2024-05-21 19:25:02 +1000

    Adding Register Functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation ("com.squareup.retrofit2:converter-simplexml:2.11.0")
     implementation ("com.google.android.material:material:1.2.0-alpha02")
+    implementation(files("libs/activation.jar"))
+    implementation(files("libs/additionnal.jar"))
+    implementation(files("libs/mail.jar"))
 
 }
 
