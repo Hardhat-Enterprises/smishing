00a283e5d55ef6041d2b9bdc602804e9ac7f2cef
KaylaP001 <130429239+KaylaP001@users.noreply.github.com>
2024-08-20 09:59:57 +1000

    Merge branch 'BackEndTeam' of /Users/kaykay/Documents/GitHub/smishing with conflicts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+        // Add other repositories here
+    }
+    dependencies {
+        classpath("com.chaquo.python:gradle:15.0.1")
+        // Add other classpaths here
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
