0c0daa2bba83b90bcd3536e6320c3e6c3a714b3d
gladizgregory <ggregory@deakin.edu.au>
2024-09-05 16:32:28 +1000

    Added Android Compiler Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         classpath(libs.gradle) // Android Gradle plugin
         classpath(libs.kotlin.gradle.plugin) // Kotlin Gradle plugin
         classpath(libs.python.gradle) // Chaquopy Gradle plugin
+        classpath(libs.androidx.compiler) //Android Compiler plugin
     }
 }
 
