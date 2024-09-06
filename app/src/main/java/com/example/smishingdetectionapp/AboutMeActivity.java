2757b6506c7ecb81fa61c6e165d171ca50c10231
usmanajaz <usmanajazkhan@gmail.com>
2024-09-07 02:47:30 +1000

    About Me Section Added

---
+++ app/src/main/java/com/example/smishingdetectionapp/AboutMeActivity.java
@@ -0,0 +1,12 @@
+package com.example.smishingdetectionapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutMeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me); // This layout will be created in the next step
+    }
+}
