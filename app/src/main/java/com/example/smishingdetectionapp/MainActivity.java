37f651b6eb414330fa040dfff97478a9dc5db0a4
KaylaP001 <130429239+KaylaP001@users.noreply.github.com>
2024-09-07 13:24:30 +1000

    Update MainActivity.java

--- app/src/main/java/com/example/smishingdetectionapp/MainActivity.java
+++ app/src/main/java/com/example/smishingdetectionapp/MainActivity.java
@@ -1,105 +1,94 @@
 package com.example.smishingdetectionapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.io.IOException;
+
 import com.example.smishingdetectionapp.databinding.ActivityMainBinding;
-import android.util.Log;
+import com.example.smishingdetectionapp.notifications.NotificationPermissionDialogFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
-/** @noinspection deprecation*/
 public class MainActivity extends AppCompatActivity {
-
-    private static final int PICK_IMAGE_REQUEST = 1;
     private AppBarConfiguration mAppBarConfiguration;
-    private ImageView imageView;
-    private TextView textView;
-    private OCRProcessor ocrProcessor;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        com.example.smishingdetectionapp.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Initialize UI components
-        imageView = findViewById(R.id.imageView);
-        textView = findViewById(R.id.textView);
-        ocrProcessor = new OCRProcessor(this);
-
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show());
-
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_news, R.id.nav_settings)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
 
-        // Set up button click listener to select an image
-        binding.appBarMain.fab.setOnClickListener(view -> openImagePicker());
-    }
-
-    private void openImagePicker() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
-            processSelectedImage(imageUri);
+        if (!areNotificationsEnabled()) {
+            showNotificationPermissionDialog();
         }
+
+        BottomNavigationView nav = findViewById(R.id.bottom_navigation);
+        nav.setSelectedItemId(R.id.nav_home);
+        nav.setOnItemSelectedListener(menuItem -> {
+            int id = menuItem.getItemId();
+            if (id == R.id.nav_home) {
+                return true;
+            } else if (id == R.id.nav_news) {
+                startActivity(new Intent(getApplicationContext(), NewsActivity.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            } else if (id == R.id.nav_settings) {
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            }
+            return false;
+        });
+
+        Button debug_btn = findViewById(R.id.debug_btn);
+        debug_btn.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, DebugActivity.class)));
+
+        Button detections_btn = findViewById(R.id.detections_btn);
+        detections_btn.setOnClickListener(v -> {
+            startActivity(new Intent(this, DetectionsActivity.class));
+            finish();
+        });
+
+        // Database connection
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
+        databaseAccess.open();
+
+        // Setting counter from the result
+        TextView total_count = findViewById(R.id.total_counter);
+        total_count.setText("" + databaseAccess.getCounter());
+
+        // Closing the connection
+        databaseAccess.close();
+
     }
 
-    private void processSelectedImage(Uri imageUri) {
-        try {
-            // Display the selected image
-            Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-            imageView.setImageBitmap(bitmap);
+    private boolean areNotificationsEnabled() {
+        return NotificationManagerCompat.from(this).areNotificationsEnabled();
+    }
 
-            // Use OCRProcessor to process the image and translate the text
-            ocrProcessor.processImage(imageUri, "en", result -> {
-                // Display the extracted and translated text
-                textView.setText(result);
-            });
-        } catch (IOException e) {
-            Log.e("MainActivity", "Failed to process image", e);
-            Toast.makeText(this, "Failed to process image", Toast.LENGTH_SHORT).show();
-        }
+    private void showNotificationPermissionDialog() {
+        NotificationPermissionDialogFragment dialogFragment = new NotificationPermissionDialogFragment();
+        dialogFragment.show(getSupportFragmentManager(), "notificationPermission");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -107,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
