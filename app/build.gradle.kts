a286f787c4d25d981baf67ecbb95a14159d997ab
gladizgregory <ggregory@deakin.edu.au>
2024-09-08 14:46:34 +1000

    Added further dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.chaquo.python")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -11,13 +12,13 @@ android {
 
     buildFeatures {
         buildConfig = true
+        viewBinding = true
+        compose = true
     }
-
 
     defaultConfig {
         ndk {
-            // On Apple silicon, you can omit x86_64.
-            abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
         }
         applicationId = "com.example.smishingdetectionapp"
         minSdk = 24
@@ -27,17 +28,12 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "EMAIL", "\"smsphishing8@gmail.com\"")
-        buildConfigField("String", "EMAILPASSWORD", "\"xedr gaek jdsv ujxw\"")
-        buildConfigField("String", "SERVERIP", "\"http:192.168.?.?:3000\"")
+        buildConfigField("String", "EMAIL_PASSWORD", "\"xedr gaek jdsv ujxw\"")
+        buildConfigField("String", "SERVERIP", "\"http://192.168.?.?:3000\"")
         vectorDrawables {
             useSupportLibrary = true
         }
-
-
-
-
-
-   }
+    }
 
     buildTypes {
         release {
@@ -45,26 +41,20 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-//    ndk {
-//        abiFilters("armeabi-v7a", "x86")
-//    }
-    compileOptions {
 
+    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-
-
     }
-    buildFeatures {
-        viewBinding = true
-        compose = true
-    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "2.0.0"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -72,7 +62,8 @@ android {
     }
 
     sourceSets {
-        getByName("main").java.srcDirs("src/main/python")
+        getByName("main").java.srcDirs("src/main/java")
+        getByName("main").resources.srcDirs("src/main/resources")
     }
 }
 
@@ -84,36 +75,14 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-    implementation(libs.annotation)
-    implementation(libs.preference)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
     implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    implementation(libs.activity)
-    implementation(files("libs/sqliteassethelper-2.0.1.jar"))
+    implementation(files("libs/activation.jar"))
+    implementation(files("libs/mail.jar"))
+    implementation(libs.preference)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
-    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation ("com.squareup.retrofit2:converter-simplexml:2.11.0")
-    implementation ("com.google.android.material:material:1.2.0-alpha02")
-    implementation(files("libs/activation.jar"))
-    implementation(files("libs/additionnal.jar"))
-    implementation(files("libs/mail.jar"))
-
-
-
-
-
+    implementation(libs.javax.mail.api)
+    implementation(libs.gson)
 }
-
