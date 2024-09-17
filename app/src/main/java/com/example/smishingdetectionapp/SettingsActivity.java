87cf912aea9bb477d5e83c0b736d65f04e1b8486
manubalhara21 <162848401+manubalhara21@users.noreply.github.com>
2024-09-17 16:38:56 +1000

    I have worked on the FEEDBACK SCREEN and COMMUNITY FORUM page.

--- app/src/main/java/com/example/smishingdetectionapp/SettingsActivity.java
+++ app/src/main/java/com/example/smishingdetectionapp/SettingsActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.smishingdetectionapp.news.NewsAdapter;
 import com.example.smishingdetectionapp.ui.account.AccountActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -35,11 +33,7 @@ public class SettingsActivity extends AppCompatActivity {
                 overridePendingTransition(0,0);
                 finish();
                 return true;
-            } else if (id == R.id.nav_settings) {
-                return true;
-            }
-
-            return false;
+            } else return id == R.id.nav_settings;
         });
 
         //Account button to switch to account page
@@ -48,7 +42,7 @@ public class SettingsActivity extends AppCompatActivity {
             startActivity(new Intent(this, AccountActivity.class));
             finish();
         });
-        //Filtering button to switch to smishing rules page
+        //Filtering button to switch to Smishing rules page
         Button filteringBtn = findViewById(R.id.filteringBtn);
         filteringBtn.setOnClickListener(v -> {
             startActivity(new Intent(this, SmishingRulesActivity.class));
@@ -73,6 +67,20 @@ public class SettingsActivity extends AppCompatActivity {
             Intent intent = new Intent(SettingsActivity.this, AboutMeActivity.class);
             startActivity(intent);
         });
+
+        //Feedback Button to switch to Feedback page
+        Button feedbackBtn = findViewById(R.id.feedbackBtn);
+        feedbackBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, FeedbackActivity.class));
+            finish();
+        });
+        //Forum Button to switch to Forum page
+        Button forumBtn = findViewById(R.id.forumBtn);
+        forumBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, ForumActivity.class));
+            finish();
+        });
+
     }
 
     //Notification button to switch to notification page
