475f83b62c6e08ff224114a596307a818685a37d
k-donghwan <162737447+k-donghwan@users.noreply.github.com>
2024-09-29 06:28:01 +1000

    Update MyService.java

--- app/src/main/java/com/example/smishingdetectionapp/MyService.java
+++ app/src/main/java/com/example/smishingdetectionapp/MyService.java
@@ -6,25 +6,21 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
-
-
-
-//    private static final String TAG = "98888";
-
+    //    private static final String TAG = "";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         AccessControl accessControl = new AccessControl();
-        String userId = "123456"; // 현재 사용자 ID
+        String userId = "kimdongh@deakin.edu.au";
         String requiredPermission = "START_BACKGROUND_SERVICE";
 
         if (accessControl.hasPermission(userId, requiredPermission)) {
-            // 권한이 있을 경우 서비스 실행
-            Log.d("MyService", "서비스 시작: 권한이");
+
+            Log.d("MyService", "Service Start: you have permission ");
         } else {
-            // 권한이 없을 경우 서비스 종료
-            Log.d("MyService", "서비스 종료: 권한이.");
-            stopSelf(); // 서비스 종료
+
+            Log.d("MyService", "Service end: you don't have permission.");
+            stopSelf();
         }
         return START_NOT_STICKY;
     }
