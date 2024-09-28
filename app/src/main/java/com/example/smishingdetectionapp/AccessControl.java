a61a3c776fb3b896cefcd95a3df39c4cd1f5466e
k-donghwan <162737447+k-donghwan@users.noreply.github.com>
2024-09-29 06:25:41 +1000

    Update AccessControl.java

--- app/src/main/java/com/example/smishingdetectionapp/AccessControl.java
+++ app/src/main/java/com/example/smishingdetectionapp/AccessControl.java
@@ -1,44 +1,5 @@
 package com.example.smishingdetectionapp;
 
-//import com.mongodb.client.MongoCollection;
-//import com.mongodb.client.MongoDatabase;
-//import org.bson.Document;
-//import static com.mongodb.client.model.Filters.eq;
-
-//public class AccessControl {
-//    private MongoCollection<Document> usersCollection;
-//    private MongoCollection<Document> rolesCollection;
-
-//    public AccessControl() {
-        // MongoDB 연결 가져오기
-        //MongoDatabase database = DB_Connection.js.getDatabase();  // 수정된 DBConnection 사용
-        //usersCollection = database.getCollection("users");
-        //rolesCollection = database.getCollection("roles");
-//    }
-
-    // 사용자가 권한을 가지고 있는지 확인하는 메서드
-//    public boolean hasPermission(String email, String requiredPermission) {
-//        Document userDoc = usersCollection.find(eq("email", email)).first();
-//        if (userDoc == null) {
-//            System.out.println("사용자를 찾을 수 없습니다.");
-//            return false;
-//        }
-
- //       String roleId = userDoc.getString("roleId");
- //       Document roleDoc = rolesCollection.find(eq("_id", roleId)).first();
- //       if (roleDoc == null) {
- //           System.out.println("역할을 찾을 수 없습니다.");
-  //          return false;
-   //     }
-
-//        Role role = Role.fromDocument(roleDoc);
-//        return role.getPermissions().contains(requiredPermission);
-//    }
-//}
-
-
-
-//import com.mongodb.MongoTimeoutException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -53,7 +14,7 @@ public class AccessControl {
     public AccessControl() {
         // MongoDB connection setting
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:3000");
-        MongoDatabase database = mongoClient.getDatabase("yourDatabaseName"); // 데이터베이스 이름 변경
+        MongoDatabase database = mongoClient.getDatabase("detectlist.db");
         rolesCollection = database.getCollection("roles");
     }
 
