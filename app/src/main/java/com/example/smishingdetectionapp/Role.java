5db89f6966633b8197c7421b317aa72a1d973861
k-donghwan <162737447+k-donghwan@users.noreply.github.com>
2024-09-29 06:28:26 +1000

    Update Role.java

--- app/src/main/java/com/example/smishingdetectionapp/Role.java
+++ app/src/main/java/com/example/smishingdetectionapp/Role.java
@@ -1,40 +1,5 @@
 package com.example.smishingdetectionapp;
 
-//import org.bson.Document;
-//import java.util.List;
-
-//public class Role {
-//    private String roleName;
-//    private List<String> permissions;
-
-    // 생성자
-//    public Role(String roleName, List<String> permissions) {
-//        this.roleName = roleName;
-//        this.permissions = permissions;
-//    }
-
-    // Role 객체를 MongoDB의 Document로 변환
-//    public Document toDocument() {
-//        return new Document("roleName", roleName)
-//                .append("permissions", permissions);
-//    }
-
-    // MongoDB Document에서 Role 객체로 변환
-//    public static Role fromDocument(Document doc) {
-//        String roleName = doc.getString("roleName");
-//        List<String> permissions = (List<String>) doc.get("permissions");
-//        return new Role(roleName, permissions);
-//    }
-
-//    public String getRoleName() {
-//        return roleName;
-//    }
-
-//    public List<String> getPermissions() {
-//        return permissions;
-//    }
-//}
-
 
 import java.util.List;
 
@@ -58,13 +23,3 @@ public class Role {
 
 
 }
-
-// 역할 및 권한 예시
-//List<String> adminPermissions = Arrays.asList("READ", "WRITE", "DELETE");
-//Role adminRole = new Role("Admin", adminPermissions);
-
-//List<String> editorPermissions = Arrays.asList("READ", "WRITE");
-//Role editorRole = new Role("Editor", editorPermissions);
-
-//List<String> viewerPermissions = Arrays.asList("READ");
-//Role viewerRole = new Role("Viewer", viewerPermissions);
