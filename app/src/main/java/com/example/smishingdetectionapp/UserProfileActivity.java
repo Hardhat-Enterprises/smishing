6e4e54c05063e6b22e2dba6bf5172e9b860bc37a
IMALKA DILUSHA <imalka.thomian@gmail.com>
2024-11-25 18:26:04 +1100

    imalka

---
+++ app/src/main/java/com/example/smishingdetectionapp/UserProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.smishingdetectionapp;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smishingdetectionapp.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile); // Replace with your layout name
+    }
+}
+
