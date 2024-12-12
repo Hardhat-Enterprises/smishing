528f0060a2b3b986947ebd3e096f2cee86c98440
Lakindu <s224222965@deakin.edu.au>
2024-12-13 10:21:46 +1100

    i did the new user tutorial page

---
+++ app/src/main/java/com/example/smishingdetectionapp/TutorialPage2.java
@@ -0,0 +1,12 @@
+package com.example.smishingdetectionapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorialPage2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_page2); // Ensure this matches your XML layout file
+    }
+}
